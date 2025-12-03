3549855e9d9a2f94f8613f48eac499e358da22a0
jjoellie <juniorlekeakem@gmail.com>
2025-12-03 21:52:42 +0100

    update Room.h

--- Smart-home/Room.h
+++ Smart-home/Room.h
@@ -7,3 +7,24 @@
 #include <iostream>
 
 #include "Device.h"
+
+class Room
+{
+public:
+    Room(const std::string& name) : name_(name) {}
+
+    std::string getName() const
+    {
+        return name_;
+    }
+
+    // Composition: Room BEZIT de devices
+    void addDevice(std::unique_ptr<Device> device)
+    {
+        devices_.push_back(std::move(device));
+    }
+
+    const std::vector<std::unique_ptr<Device>>& getDevices() const
+    {
+        return devices_;
+    }
