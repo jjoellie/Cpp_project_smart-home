0b633039ca1f354f25fafe5f30e96eabce069bed
jjoellie <juniorlekeakem@gmail.com>
2025-12-04 19:58:25 +0100

    Rooms aangemaakt en devices aan rooms toegevoegd

--- Smart-home/main.cpp
+++ Smart-home/main.cpp
@@ -1,11 +1,19 @@
-#include "mainwindow.h"
+#include <iostream>
+#include <memory>
 
-#include <QApplication>
+#include "SmartHome.h"
+#include "Devices.h"
 
-int main(int argc, char *argv[])
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+    SmartHome home("Joel");
+
+    Room livingRoom("Living Room");
+    Room bedroom("Bedroom");
+
+    livingRoom.addDevice(std::make_unique<Light>("Ceiling Light", "Living Room"));
+    livingRoom.addDevice(std::make_unique<Thermostat>("Main Thermostat", "Living Room", 22.5));
+    livingRoom.addDevice(std::make_unique<MotionSensor>("Entrance Sensor", "Living Room"));
+
+    bedroom.addDevice(std::make_unique<Light>("Bedside Lamp", "Bedroom"));
+    bedroom.addDevice(std::make_unique<Thermostat>("Bedroom Thermostat", "Bedroom", 20.0));
