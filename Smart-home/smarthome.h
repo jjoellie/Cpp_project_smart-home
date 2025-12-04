66470402134b5f5cf3cc21cea9d75770ffc1433c
jjoellie <juniorlekeakem@gmail.com>
2025-12-04 19:54:21 +0100

    finished smarthome.h

--- Smart-home/smarthome.h
+++ Smart-home/smarthome.h
@@ -8,3 +8,47 @@
 #include "Room.h"
 
 class SmartHome
+{
+public:
+    SmartHome(const std::string& ownerName)
+        : ownerName_(ownerName)
+    {
+    }
+
+    void addRoom(const Room& room)
+    {
+        rooms_.push_back(room);
+    }
+
+    std::vector<Room>& getRooms()
+    {
+        return rooms_;
+    }
+
+    const std::vector<Room>& getRooms() const
+    {
+        return rooms_;
+    }
+
+    void updateAll()
+    {
+        for (auto& room : rooms_)
+            room.updateDevices();
+    }
+
+    void printOverview() const
+    {
+        std::cout << "SmartHome of " << ownerName_ << "\n";
+        for (const auto& room : rooms_)
+        {
+            room.printDevices();
+            std::cout << "\n";
+        }
+    }
+
+private:
+    std::string ownerName_;
+    std::vector<Room> rooms_;
+};
+
+#endif
