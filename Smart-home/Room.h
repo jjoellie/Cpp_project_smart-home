c436ef3e23dc26ca94d5be9a35e5664e7443b276
jjoellie <juniorlekeakem@gmail.com>
2025-12-21 19:04:47 +0100

    added comments

--- Smart-home/Room.h
+++ Smart-home/Room.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <vector>
 #include <memory>
-
 #include "Devices.h"
 
 namespace smarthome
@@ -11,31 +10,28 @@ namespace smarthome
 class Room
 {
 public:
+    // Vraag 12: default constructor
     Room() : name_("Unknown") {}
+
+    // Vraag 13: parameterized constructor
     explicit Room(const std::string& name) : name_(name) {}
 
+    // Vraag 14: copy constructors intentionally disabled
     Room(const Room&) = delete;
     Room& operator=(const Room&) = delete;
 
+    // Move allowed
     Room(Room&&) = default;
     Room& operator=(Room&&) = default;
 
     ~Room() = default;
 
-    const std::string& getName() const
-    {
-        return name_;
-    }
+    // Vraag 23 & 29: const references
+    const std::string& getName() const { return name_; }
 
-    std::vector<std::unique_ptr<Device>>& getDevices()
-    {
-        return devices_;
-    }
-
-    const std::vector<std::unique_ptr<Device>>& getDevices() const
-    {
-        return devices_;
-    }
+    // Vraag 36: container usage
+    std::vector<std::unique_ptr<Device>>& getDevices() { return devices_; }
+    const std::vector<std::unique_ptr<Device>>& getDevices() const { return devices_; }
 
     void addDevice(std::unique_ptr<Device> device)
     {
